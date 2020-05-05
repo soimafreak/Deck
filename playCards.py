3e9d18a49a2b217f233b55b92d07a734642cd0d0
a1551256 <a1551256@03HW057164.local>
2020-05-05 13:44:44 +0100

    Added Dealing / Returning cards to decks

--- playCards.py
+++ playCards.py
@@ -3,11 +3,25 @@
 
 from deck import Deck
 
+print("Create a deck and a hand")
 myDeck = Deck()
 myDeck.show()
-
 myHand = Deck("hand")
 myHand.show()
 
+print("Shuffle the deck")
 myDeck.shuffle()
 myDeck.show()
+
+print("Check the Hand is empty and the Deck is full")
+print("myDeck has {} cards".format(len(myDeck.cards)))
+print("myHand has {} cards".format(len(myHand.cards)))
+
+print("Deal 7 cards to the hand")
+for i in range(0,7):
+    myHand.return_card(myDeck.deal_card())
+
+print("myDeck has {} cards".format(len(myDeck.cards)))
+myDeck.show()
+print("myHand has {} cards".format(len(myHand.cards)))
+myHand.show()
